4f4653450252952b8eb526dd7cf0af8a197cd3b0
Marvindiff <pamintuanmarvin0109@gmail.com>
2026-05-13 18:20:41 +0800

    Update landing.blade.php

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,7 +7,7 @@
 
     <title>PawPal • Pet Care </title>
 
-    @vite(['resources/css/app.css'])
+    @vite(['resources/js/app.js'])
 
     <!-- FONT -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
