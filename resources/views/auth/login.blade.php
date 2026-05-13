8e8ebf51454841588b323ee02ae29b1d7c44a3a3
Marvindiff <pamintuanmarvin0109@gmail.com>
2026-05-13 18:49:32 +0800

    Use Tailwind CDN instead of Vite

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,9 @@
 
     <title>PawPal • Login</title>
 
-    @vite('resources/css/app.css')
+   
+
+<script src="https://cdn.tailwindcss.com"></script>
 
     <!-- FONT -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
