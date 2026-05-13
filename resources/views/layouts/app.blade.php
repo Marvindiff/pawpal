cf4eb619ae36b4d6662aab6ea384661e940109c6
Marvindiff <pamintuanmarvin0109@gmail.com>
2026-05-13 20:08:55 +0800

    Fix Vite assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PawPal Dashboard</title>
 
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased bg-gradient-to-br from-indigo-200 via-purple-200 to-pink-200 min-h-screen">
